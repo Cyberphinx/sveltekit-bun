d88fc0c06d1349faa3bec0480f706820177446f5
cyberphinx <88593809+Cyberphinx@users.noreply.github.com>
2024-03-24 15:21:46 +0000

    :sparkles: added arcgis-esri

---
+++ src/routes/Map.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    onMount(async () => {
+        const Map = (await import("@arcgis/core/Map")).default;
+        const MapView = (await import("@arcgis/core/views/MapView")).default;
+
+        const map = new Map({
+            basemap: "gray-vector",
+        });
+
+        const view = new MapView({
+            container: "viewDiv",
+            map: map,
+        });
+
+        view.when(() => {
+            console.debug("Map loaded");
+        });
+    });
+</script>
+
+<div id="viewDiv" />
+
+<style>
+    @import "@arcgis/core/assets/esri/themes/light/main.css";
+
+    #viewDiv {
+        min-height: 500px;
+    }
+</style>
